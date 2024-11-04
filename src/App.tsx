4a0fc70e63787893b7e0986cb24cc96399ff357f
MURATA Mitsuharu <hikari.photon+dev@gmail.com>
2024-11-04 19:32:09 +0900

    ストリーミングレスポンス

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,15 @@ import Cookies from 'js-cookie';
 import { useTranslation } from 'react-i18next';
 import "./i18n"
 import { ChatCompletionMessageParam } from 'openai/resources/chat/completions';
+import { v4 as uuidv4 } from 'uuid';
 
 const getSystemTheme = (): string => {
   const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
   return darkThemeMq.matches ? "dark" : "light";
+}
+
+const generateUUID = (): string => {
+  return uuidv4();
 }
 
 interface User {
@@ -25,21 +30,48 @@ interface Author {
   label: string
 }
 
-interface Message{
+interface Message {
   role: string,
   content: string
 }
 
+interface Conversation {
+  uuid: string
+  user: string
+  assistant: string
+}
+
 interface HistoryItem {
-  userTitle: User
+  userTitle: User,
+  uuid: string
 }
 
 interface HistoryProps {
   items: HistoryItem[]
   width: number
+  conversations: Conversation[]
+  setMarkdown: React.Dispatch<React.SetStateAction<string>>
+  setAssistant: React.Dispatch<React.SetStateAction<string>>
 }
 
-export const History: React.FC<HistoryProps> = ({ items, width }) => {
+const clickHistory = (
+  uuid: string,
+  conversations: Conversation[],
+  setMarkdown: React.Dispatch<React.SetStateAction<string>>,
+  setAssistant: React.Dispatch<React.SetStateAction<string>>
+) => {
+  const contents: Conversation[] = conversations.filter(e => e.uuid === uuid)
+  if (contents.length > 0) {
+    const content: Conversation = contents[0]
+    const user = content.user
+    const assistant = content.assistant
+    setMarkdown(user)
+    setAssistant(assistant)
+    console.log(user, assistant)
+  }
+}
+
+const History: React.FC<HistoryProps> = ({ items, width, conversations, setMarkdown, setAssistant }) => {
   const { t } = useTranslation()
 
   const columns =
@@ -62,7 +94,7 @@ export const History: React.FC<HistoryProps> = ({ items, width }) => {
         {items.map((item) => (
           <TableRow key={item.userTitle.label}>
             <TableCell>
-              <TableCellLayout truncate>
+              <TableCellLayout style={{ userSelect: "none" }} truncate id={item.uuid} onClick={() => clickHistory(item.uuid, conversations, setMarkdown, setAssistant)}>
                 {item.userTitle.label}
               </TableCellLayout>
             </TableCell>
@@ -75,46 +107,67 @@ export const History: React.FC<HistoryProps> = ({ items, width }) => {
 
 const apiKey = Cookies.get('OPENAI_API_KEY');
 let openai: OpenAI | null = null
-if(apiKey){
+if (apiKey) {
   openai = new OpenAI({
     apiKey: apiKey,
     dangerouslyAllowBrowser: true
   });
 }
 
-const Send = async(
-  markdown: string, 
-  setAssistant: React.Dispatch<React.SetStateAction<string>>, 
-  historyItems: HistoryItem[], 
+const getStreamingResponse = async (
+  openai: OpenAI | null,
+  messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[],
+  setAssistant: React.Dispatch<React.SetStateAction<string>>
+): Promise<string | null> => {
+  if (openai == null)
+    return null
+  const stream = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: messages,
+    stream: true,
+  })
+
+  let responseText = ""
+  for await (const chunk of stream) {
+    const output: string = chunk.choices[0]?.delta?.content || ""
+    console.log(output)
+    responseText += output
+    setAssistant(responseText)
+  }
+  return responseText
+}
+
+const Send = async (
+  markdown: string,
+  setAssistant: React.Dispatch<React.SetStateAction<string>>,
+  historyItems: HistoryItem[],
   setItems: React.Dispatch<React.SetStateAction<HistoryItem[]>>,
   progressRef: React.MutableRefObject<null>,
   messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[],
-  setMessages: React.Dispatch<React.SetStateAction<OpenAI.Chat.Completions.ChatCompletionMessageParam[]>>
+  setMessages: React.Dispatch<React.SetStateAction<OpenAI.Chat.Completions.ChatCompletionMessageParam[]>>,
+  conversations: Conversation[],
+  setConversations: React.Dispatch<React.SetStateAction<Conversation[]>>
 ) => {
   console.log("SEND:", markdown)
-  if(openai != null){
+  if (openai != null) {
     const userMessage: ChatCompletionMessageParam = {
       role: "user",
       content: markdown
     }
     const requestMessages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [...messages, userMessage]
     // プログレスバーの表示
-    if(progressRef.current){
+    if (progressRef.current) {
       const current: HTMLElement = progressRef.current
       current.classList.remove("hidden")
     }
     // 送信
-    const chatCompletion = await openai.chat.completions.create({
-      messages: requestMessages,
-      model: "gpt-4o-mini",
-    })
+    const assistant = await getStreamingResponse(openai, requestMessages, setAssistant)
     // プログレスバーの非表示
-    if(progressRef.current){
+    if (progressRef.current) {
       const current: HTMLElement = progressRef.current
       current.classList.add("hidden")
     }
-    const assistant = chatCompletion.choices[0].message.content
-    if(assistant){
+    if (assistant) {
       // Message リストに追加
       const assistantMessage: ChatCompletionMessageParam = {
         role: "assistant",
@@ -122,12 +175,21 @@ const Send = async(
       }
       setMessages([...requestMessages, assistantMessage])
 
+      const uuid: string = generateUUID()
+      const conversation: Conversation = {
+        uuid: uuid,
+        user: markdown,
+        assistant: assistant
+      }
+      setConversations([...conversations, conversation])
+
       // Markdown を表示
       setAssistant(assistant)
 
       const user1line = markdown.split("\n")[0]
       const historyItem: HistoryItem = {
         userTitle: { label: user1line },
+        uuid: uuid
       }
 
       // 履歴アイテムに追加
@@ -137,6 +199,8 @@ const Send = async(
 }
 
 const App = () => {
+  const uuid: string = generateUUID()
+  const [conversations, setConversations] = useState<Conversation[]>([])
   const [messages, setMessages] = useState<ChatCompletionMessageParam[]>([])
   const { t } = useTranslation()
   const theme = getSystemTheme() === "light" ? webLightTheme : webDarkTheme;
@@ -159,19 +223,19 @@ const App = () => {
   return (
     <FluentProvider theme={theme}>
       <div className="App">
-        <ProgressBar ref={progressRef} style={{height: '4px'}} className="hidden" id="progress-bar" />
+        <ProgressBar ref={progressRef} style={{ height: '4px' }} className="hidden" id="progress-bar" />
         <main>
           <div id="editor">
             <div style={{ borderRadius: '4px', overflow: 'hidden' }}>
-              <Editor height="calc(100vh - 72px)" defaultLanguage="markdown" value={markdown} theme={vsTheme} onChange={handleEditorChange} />
+              <Editor options={{ wordWrap: 'on' }} height="calc(100vh - 72px)" defaultLanguage="markdown" value={markdown} theme={vsTheme} onChange={handleEditorChange} />
             </div>
-            <Button id="send-button" appearance="primary" onClick={() => Send(markdown, setAssistant, historyItems, setHistoryItems, progressRef, messages, setMessages)} style={{ width: '100%' }} icon={<SendFilled />}>{t('send')}</Button>
+            <Button id="send-button" appearance="primary" onClick={() => Send(markdown, setAssistant, historyItems, setHistoryItems, progressRef, messages, setMessages, conversations, setConversations)} style={{ width: '100%' }} icon={<SendFilled />}>{t('send')}</Button>
           </div>
           <div id="preview">
             <Markdown children={assistant} />
           </div>
           <div id="history">
-            <History items={historyItems} width={historyWidth} />
+            <History items={historyItems} width={historyWidth} conversations={conversations} setMarkdown={setMarkdown} setAssistant={setAssistant} />
           </div>
         </main>
       </div>
